a34dfb78e0006c002a3948a2486637bfcf446a5f
a-pillow <kaluhblee@gmail.com>
2023-04-08 11:33:57 -0500

    added tooltip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,20 @@ import { useState } from "react";
 import { InteractiveMap } from "./components/InteractiveMap";
 import { Navbar } from "./components/Navbar";
 import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 
 function App() {
   const [country, setCountry] = useState("");
   return (
     <div className="h-screen bg-orange-200">
       <Navbar />
-      <InteractiveMap setTooltip={setCountry} />
-      <Tooltip id="tooltip">{country}</Tooltip>
-      <h1 data-tooltip-id="tooltip" data-tooltip-content={country}>
-        {country}
-      </h1>
+      <InteractiveMap
+        data-tooltip-id="tooltip"
+        data-tooltip-content="HELLO"
+        setTooltip={setCountry}
+      />
+      <Tooltip id="tooltip" />
+      <a>{country}</a>
     </div>
   );
 }

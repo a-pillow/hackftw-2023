4561b6f15f8201a846b910ae87a0bf1c86555e5c
a-pillow <kaluhblee@gmail.com>
2023-04-08 10:03:53 -0500

    navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,8 +2,13 @@
 
 export function Navbar() {
     return (
-        <h1>
-            hi
-        </h1>
+        <nav className="w-full py-4 px-16 flex items-center ">
+            <h1 className="font-bold text-4xl mr-16">
+                placeholder name
+            </h1>
+            <div className="w-full text-black">
+                <input className="border-b-2 border-black placeholder-black bg-orange-200 w-1/6 outline-none focus:w-1/2 duration-200" type="text" placeholder="Search country..." />
+            </div>
+        </nav>
     )
 }
